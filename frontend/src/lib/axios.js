6a3df1540cc35f034786545fa3aef8f57f7fd174
mohammed1819 <mtaha_cse2305a2@mgit.ac.in>
2026-06-14 20:52:40 +0530

    added backend deployed link to frontend axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://webchat-4fs4.onrender.com",
   withCredentials: true,
 });
